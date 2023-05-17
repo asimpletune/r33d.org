5944fb26a57cc209f565ed375f8c435c7e35af57
Spencer Scorcelletti <contact@spenc.es>
2023-05-17 17:00:55 +0200

    Streamline front page summary of readings

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,24 @@ module.exports = {
         'playfair-display': ['Playfair Display', ...defaultTheme.fontFamily.serif],
         'fira-sans': ['Fira Sans', ...defaultTheme.fontFamily.sans],
       },
+
+      /**
+       * Notes:
+       * 1. `DEFAULT` and `minutes` are color themes, see https://tailwindcss.com/docs/typography-plugin#adding-custom-color-themes
+       * 2. the default is applied to everything, and `minutes` must be used explicitly (search CSS class 'prose-minutes' to find its use)
+       * 3. however it's preferred to use 'element modifiers' directly in the html if you can, see https://tailwindcss.com/docs/typography-plugin#element-modifiers
+       * 4. typography internal style definitions https://github.com/tailwindlabs/tailwindcss-typography/blob/master/src/styles.js
+       */
       typography: ({ theme }) => ({
-        /**
-         * Notes:
-         * 1. 'minutes' is a custom 'color theme', see https://tailwindcss.com/docs/typography-plugin#adding-custom-color-themes
-         * 2. it's used for customizing the 'meeting minutes' markdown, search CSS class 'prose-minutes' to find its use
-         * 3. however it's preferred to use 'element modifiers' directly in the html if you can, see https://tailwindcss.com/docs/typography-plugin#element-modifiers
-         * 4. typography internal style definitions https://github.com/tailwindlabs/tailwindcss-typography/blob/master/src/styles.js
-         */
-        minutes: {
+        DEFAULT: {
           css: {
             '--tw-prose-bullets': theme('colors.pink[400]'),
-            '--tw-prose-hr': theme('colors.violet[400]'),
+            '--tw-prose-counters': theme('colors.pink[400]'),
             '--tw-prose-links': theme('colors.blue[600]'),
+          }
+        },
+        minutes: {
+          css: {
             h3: { '--tw-prose-links': theme('colors.black') },
             '.zola-anchor': {
               color: theme('colors.green[500]'),
